d5ebb0b4ff32d1c6b62f51598823f631aaed5555
alvinjamal <alvinjamalazkya@gmail.com>
2023-02-20 00:38:45 +0700

    Update cors

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ const mainRouter = require("./src/routes/index");
 const app = express();
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
-app.use(cors({ origin: true, credentials: true }));
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+//   })
+// );
+const corsOptions = {
+  origin: "https://frontend-food-recipe.vercel.app",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
