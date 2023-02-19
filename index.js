c56e0625f8b5487b7a9757b8338133286301499b
alvinjamal <alvinjamalazkya@gmail.com>
2023-02-19 15:56:19 +0700

    Deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://food-recipe-alvinjamal.vercel.app",
   })
 );
 app.use(cors({ origin: true, credentials: true }));
