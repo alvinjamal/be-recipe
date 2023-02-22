bf96a907220e58b89e10e73b5a3403d8277d48ed
alvinjamal <alvinjamalazkya@gmail.com>
2023-02-22 20:16:57 +0700

    Update

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -106,6 +106,7 @@ const UsersController = {
     delete users.otp;
     delete users.verif;
     let payload = {
+      token: users.token,
       id_user: users.id_user,
       email: users.email,
     };
