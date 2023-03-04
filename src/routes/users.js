b9abaa738e6083e32bf6334cc8e02d79c253e82d
alvinjamal <alvinjamalazkya@gmail.com>
2023-03-04 08:34:17 +0700

    finish user

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,7 +13,19 @@ router.post("/verification", UsersController.verificationOtp);
 router.post("/forgot", UsersController.forgotPassword);
 router.post("/forgot/:token", UsersController.resetPassword);
 
-router.put("/update", protect, upload, UsersController.updatePhoto);
+router.put(
+  "/update",
+  protect,
+  upload,
+  UsersController.updatePhoto,
+  function (req, res) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://food-recipe-alvinjamal.vercel.app"
+    );
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+);
 // GET
 router.get("/", UsersController.getDataAll);
 router.get("/profile", protect, UsersController.getProfile);
